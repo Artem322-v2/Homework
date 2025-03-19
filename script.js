70734a65a7cc46c710629e8b4edc160ba7a959ae
Artem322-v2 <zakiev2100@mail.ru>
2025-03-19 15:30:34 +0400

    Тема: Компоненты языка JavaScript

---
+++ script.js
@@ -0,0 +1,13 @@
+let cityName = "Ижевск"; // Присвоил название города
+ 
+let temperature = "2°С"; // Присвоил значение температуры
+
+console.log(cityName, "-", temperature); // Выводит название города и температуру
+
+
+let a = 30; // Присвоил значение a,b и c
+let b = 11;
+let c = 15;
+
+const result = a + b / c; // Выполнил выражение
+console.log(result); // Вывел результат в консоль
